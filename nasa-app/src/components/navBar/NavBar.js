631c10f5dbea9a4eac1a6f9e283f009e977e3ee4
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-12 16:21:53 +0300

    Fixed sticky navbar

--- nasa-app/src/components/navBar/NavBar.js
+++ nasa-app/src/components/navBar/NavBar.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Logo from "../logo/Logo"
 import "../../styles/navBar.css"
 import Menu from "./Menu"
 
 export default function NavBar(props) {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  const handleScroll = () => {
+    if (window.pageYOffset > 0) {
+      setIsScrolled(true)
+    } else {
+      setIsScrolled(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
   return (
-    <div id="nav-bar" className={props.isContainerScrolled ? "sticky" : null}>
+    <div id="nav-bar" className={isScrolled ? "sticky" : null}>
       <Menu activeTab={props.activeTab} />
       <Logo />
     </div>
