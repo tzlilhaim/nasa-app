df452ad27b2475b1f0d622e3a52fa9a6baf9ed5c
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-15 10:40:57 +0300

    m

--- src/index.js
+++ src/index.js
@@ -1,33 +1,17 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
-import { ReactReduxFirebaseProvider } from "react-redux-firebase"
-import { createFirestoreInstance } from "redux-firestore"
-import firebase from "./firebase"
-
-const store = createStore(rootReducer)
-
-const rrfProps = {
-  firebase,
-  config: {
-    userProfile: "users",
-  },
-  dispatch: store.dispatch,
-  createFirestoreInstance,
-}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById("root")
-)
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { firestoreReducer } from "redux-firestore"
-
-const rootReducer = combineReducers({
-  formVisibleOnPage: formVisibleReducer,
-  masterTicketList: ticketListReducer,
-  // new line of code below
-  firestore: firestoreReducer,
-})
