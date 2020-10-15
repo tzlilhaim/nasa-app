98293e676b30980fc63c09d51a3b3154844aad7f
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-15 11:56:43 +0300

    m

--- server.js
+++ server.js
@@ -22,13 +22,13 @@ app.use(function (req, res, next) {
 
 app.use("/", api)
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = config.serverPort || 5000
+const port = config.serverPort
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
