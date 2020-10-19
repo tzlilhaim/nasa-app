21b3585cc83f07885e620d24db935c96461bd42f
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-19 11:02:17 +0300

    m

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ScrollToTop from "./components/utils/ScrollToTop"
 const axios = require("axios")
 
 export default function App() {
-  const serverUrl = process.env.SERVER_URL || `http://localhost:5000/api`
+  const serverUrl = process.env.SERVER_URL
   const [snackBarOpen, setSnackBarOpen] = useState(false)
   const [snackBarMsg, setSnackBarMsg] = useState("")
   const [favourites, setFavourites] = useState([])
