0316c5fc7dc7ad9ec0653b56bbf934f2ec176e5a
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-19 09:04:30 +0300

    m

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser")
 const api = require("./src/backend/server/routes/api")
 const config = require("./config")
 const path = require("path")
-const cors = require("cors")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
@@ -12,23 +11,11 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'build')));
 app.use("/api", api)
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Content-Length, X-Requested-With"
-  )
-
-  next()
-})
-app.use(cors())
-
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = config.serverPort || 5000
+const port = config.serverPort
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
