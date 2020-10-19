4a2b66602dcbbba924e783bc507768910e90ae90
tzlilhaim <tzlilhaim480@gmail.com>
2020-10-19 09:33:29 +0300

    m

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const bodyParser = require("body-parser")
 const api = require("./src/backend/server/routes/api")
 const config = require("./config")
 const path = require("path")
+const cors = require("cors")
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, "build")))
 app.use("/api", api)
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  )
+
+  next()
+})
+app.use(cors())
 
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"))
